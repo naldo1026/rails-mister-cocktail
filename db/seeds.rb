c42fbd3269e6ceb0813d5d1b01f0ec9910b4f1a1
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-02-18 16:15:19 +0000

    mister cocktail working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Whiskey Sour")
+Cocktail.create(name: "Mint Julep")
+
+Dose.create(description: "Has vodka in")
+Dose.create(description: "Has whiskey in")
+Dose.create(description: "Has mint leaves in")
